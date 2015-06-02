27ec90290d6eb9b739a631be33c77aff45300c2a
root <root@localhost>
2015-06-02 17:18:15 +0200

    Add Fix combinator

--- src/lib.rs
+++ src/lib.rs
@@ -7,3 +7,18 @@ pub unsafe fn raw_byte_repr<'a, T>(ptr: &'a T) -> &'a [u8] {
         mem::size_of::<T>(),
     )
 }
+
+pub struct Fix<'a, T, R>(pub &'a Fn(Fix<T, R>, T) -> R);
+
+impl<'a, T, R> Fix<'a, T, R> {
+    pub fn call(&self, arg: T) -> R {
+        let f = *self;
+        f.0(f, arg)
+    }
+}
+
+impl<'a, T, R> Clone for Fix<'a, T, R> {
+    fn clone(&self) -> Self { *self }
+}
+
+impl<'a, T, R> Copy for Fix<'a, T, R> { }
