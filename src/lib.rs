dcb89bc75ad67986691adf4691cbfc2643560506
root <root@localhost>
2015-06-02 17:18:15 +0200

    Add IndexRange, add debug_assert_unreachable

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+pub use range::IndexRange;
+mod range;
+
 use std::{slice, mem};
 
 /// Safe to use with any wholly initialized memory `ptr`
@@ -8,12 +11,14 @@ pub unsafe fn raw_byte_repr<'a, T>(ptr: &'a T) -> &'a [u8] {
     )
 }
 
+/// Use `debug_assert!` to check indexing in debug mode. In release mode, no checks are done.
 #[inline]
 pub unsafe fn get_unchecked<T>(data: &[T], index: usize) -> &T {
     debug_assert!(index < data.len());
     data.get_unchecked(index)
 }
 
+/// Use `debug_assert!` to check indexing in debug mode. In release mode, no checks are done.
 #[inline]
 pub unsafe fn get_unchecked_mut<T>(data: &mut [T], index: usize) -> &mut T {
     debug_assert!(index < data.len());
@@ -34,3 +39,24 @@ impl<'a, T, R> Clone for Fix<'a, T, R> {
 }
 
 impl<'a, T, R> Copy for Fix<'a, T, R> { }
+
+/// An empty type
+pub enum Void { }
+
+/// FIXME: Replace with intrinsic when it's stable
+#[inline]
+unsafe fn unreachable() -> ! {
+    let void: &Void = mem::transmute(&());
+    match *void {
+        // no cases
+    }
+}
+
+/// Act as `debug_assert!` in debug mode, asserting that this point is not reached.
+///
+/// In release mode, no checks are done, and it acts like the `unreachable` intrinsic.
+#[inline]
+pub unsafe fn debug_assert_unreachable() -> ! {
+    debug_assert!(false, "Entered unreachable section, this is a bug!");
+    unreachable()
+}
---
+++ src/range.rs
@@ -0,0 +1,41 @@
+
+use std::ops::{
+    RangeFull,
+    RangeFrom,
+    RangeTo,
+    Range,
+};
+
+/// **IndexRange** is implemented by Rust's built-in range types, produced
+/// by range syntax like `..`, `a..`, `..b` or `c..d`.
+pub trait IndexRange {
+    #[inline]
+    #[doc(hidden)]
+    /// Start index (inclusive)
+    fn start(&self) -> Option<usize> { None }
+    #[inline]
+    #[doc(hidden)]
+    /// End index (exclusive)
+    fn end(&self) -> Option<usize> { None }
+}
+
+
+impl IndexRange for RangeFull {}
+
+impl IndexRange for RangeFrom<usize> {
+    #[inline]
+    fn start(&self) -> Option<usize> { Some(self.start) }
+}
+
+impl IndexRange for RangeTo<usize> {
+    #[inline]
+    fn end(&self) -> Option<usize> { Some(self.end) }
+}
+
+impl IndexRange for Range<usize> {
+    #[inline]
+    fn start(&self) -> Option<usize> { Some(self.start) }
+    #[inline]
+    fn end(&self) -> Option<usize> { Some(self.end) }
+}
+
