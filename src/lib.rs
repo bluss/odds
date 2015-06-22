cd7fcbbff7677e2728c4395e167e5c28bd557852
root <root@localhost>
2015-06-22 12:22:09 +0200

    Remove explicit lifetimes from raw_byte_repr
    It just looks redundant / weird now that we are used to lifetime
    elision.

--- src/lib.rs
+++ src/lib.rs
@@ -37,7 +37,8 @@ pub fn ptr_eq<T>(a: *const T, b: *const T) -> bool {
 }
 
 /// Safe to use with any wholly initialized memory `ptr`
-pub unsafe fn raw_byte_repr<'a, T>(ptr: &'a T) -> &'a [u8] {
+#[inline]
+pub unsafe fn raw_byte_repr<T>(ptr: &T) -> &[u8] {
     slice::from_raw_parts(
         ptr as *const _ as *const u8,
         mem::size_of::<T>(),
