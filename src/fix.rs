2d03bf6a6a4adfa6cb2f9edbec8c9bb67d137d49
bluss <bluss@users.noreply.github.com>
2016-09-08 14:35:01 +0200

    Add fix function

--- src/fix.rs
+++ src/fix.rs
@@ -7,20 +7,20 @@
 /// **Fix** only supports function call notation with the nightly channel and
 /// the cargo feature ‘unstable’ enabled.
 ///
+/// Use this best through the `fix` function.
+///
 /// ```
 /// use odds::Fix;
+/// use odds::fix;
 ///
 /// let c = |f: Fix<i32>, x| if x == 0 { 1 } else { x * f.call(x - 1) };
 /// let fact = Fix(&c);
 /// assert_eq!(fact.call(5), 120);
 ///
-/// let data = &[true, false];
-/// let all_true = |f: Fix<_, _>, x| {
-///     let x: &[_] = x;
+/// let data = [true, false];
+/// assert!(!fix(&data[..], |f, x| {
 ///     x.len() == 0 || x[0] && f.call(&x[1..])
-/// };
-/// let all = Fix(&all_true);
-/// assert_eq!(all.call(data), false);
+/// }));
 ///
 /// ```
 #[cfg_attr(feature="unstable", doc="
@@ -35,6 +35,46 @@ assert_eq!(fact(5), 120);
 "
 )]
 pub struct Fix<'a, T: 'a, R: 'a = T>(pub &'a Fn(Fix<T, R>, T) -> R);
+
+/// Fixpoint combinator for rust closures, generalized over the return type.
+///
+/// This is a wrapper function that uses the `Fix` type. The recursive closure
+/// has two arguments, `Fix` and the argument type `T`.
+///
+/// In **Fix\<T, R\>**, **T** is the argument type, and **R** is the return type,
+/// **R** defaults to **T**.
+///
+/// Calling the `Fix` value recursively only supports function call notation with the nightly
+/// channel and the cargo feature ‘unstable’ enabled. Otherwise use the .call()
+/// method.
+///
+/// This helper function makes the type inference work out well.
+///
+/// ```
+/// use odds::fix;
+/// 
+/// assert_eq!(120, fix(5, |f, x| if x == 0 { 1 } else { x * f.call(x - 1) }));
+///
+/// let data = [true, false];
+/// assert!(!fix(&data[..], |f, x| {
+///     x.len() == 0 || x[0] && f.call(&x[1..])
+/// }));
+///
+/// ```
+#[cfg_attr(feature="unstable", doc="
+```
+// using feature `unstable`
+use odds::fix;
+
+assert_eq!(120, fix(5, |f, x| if x == 0 { 1 } else { x * f(x - 1) }));
+```
+"
+)]
+pub fn fix<T, R, F>(init: T, closure: F) -> R
+    where F: Fn(Fix<T, R>, T) -> R
+{
+    Fix(&closure).call(init)
+}
 
 impl<'a, T, R> Fix<'a, T, R> {
     pub fn call(&self, arg: T) -> R {
