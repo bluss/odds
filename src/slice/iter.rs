b06386e5616c3ffd065ac1c6b8f7ac795c93fb53
bluss <bluss@users.noreply.github.com>
2016-11-21 18:37:04 +0100

    Implement a slice iterator

--- src/slice/iter.rs
+++ src/slice/iter.rs
@@ -1,7 +1,8 @@
-//! A slice iterator for copy elements.
+//! Slice iterators
 
 use std::mem::size_of;
 use std::marker::PhantomData;
+use std::ops::Index;
 
 /// Slice (contiguous data) iterator.
 ///
@@ -42,6 +43,16 @@ impl<'a, T> SliceCopyIter<'a, T>
     /// Return the start, end pointer of the iterator
     pub fn into_raw(self) -> (*const T, *const T) {
         (self.ptr, self.end)
+    }
+
+    /// Return the start pointer
+    pub fn start(&self) -> *const T {
+        self.ptr
+    }
+
+    /// Return the end pointer
+    pub fn end(&self) -> *const T {
+        self.end
     }
 }
 
@@ -119,11 +130,126 @@ impl<'a, T> Default for SliceCopyIter<'a, T>
     }
 }
 
-use std::ops::Index;
-
 impl<'a, T> Index<usize> for SliceCopyIter<'a, T>
     where T: Copy
 {
+    type Output = T;
+    fn index(&self, i: usize) -> &T {
+        assert!(i < self.len());
+        unsafe {
+            &*self.ptr.offset(i as isize)
+        }
+    }
+}
+
+/// Slice (contiguous data) iterator.
+///
+/// Iterator element type is `&T`
+/// This iterator exists mainly to have the constructor from a pair
+/// of raw pointers available, which the libcore slice iterator does not allow.
+///
+/// `T` must not be a zero sized type.
+#[derive(Debug)]
+pub struct SliceIter<'a, T: 'a> {
+    ptr: *const T,
+    end: *const T,
+    ty: PhantomData<&'a T>,
+}
+
+impl<'a, T> Copy for SliceIter<'a, T> { }
+impl<'a, T> Clone for SliceIter<'a, T> {
+    fn clone(&self) -> Self { *self }
+}
+
+impl<'a, T> SliceIter<'a, T> {
+    /// Create a new slice iterator
+    #[inline]
+    pub unsafe fn new(ptr: *const T, end: *const T) -> Self {
+        assert!(size_of::<T>() != 0);
+        SliceIter {
+            ptr: ptr,
+            end: end,
+            ty: PhantomData,
+        }
+    }
+
+    /// Return the start pointer
+    pub fn start(&self) -> *const T {
+        self.ptr
+    }
+
+    /// Return the end pointer
+    pub fn end(&self) -> *const T {
+        self.end
+    }
+}
+
+impl<'a, T> Iterator for SliceIter<'a, T> {
+    type Item = &'a T;
+    #[inline]
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.ptr != self.end {
+            unsafe {
+                let elt = Some(&*self.ptr);
+                self.ptr = self.ptr.offset(1);
+                elt
+            }
+        } else {
+            None
+        }
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        let len = (self.end as usize - self.ptr as usize) / size_of::<T>();
+        (len, Some(len))
+    }
+
+    fn count(self) -> usize {
+        self.len()
+    }
+
+    fn last(mut self) -> Option<Self::Item> {
+        self.next_back()
+    }
+}
+
+impl<'a, T> DoubleEndedIterator for SliceIter<'a, T> {
+    #[inline]
+    fn next_back(&mut self) -> Option<Self::Item> {
+        if self.ptr != self.end {
+            unsafe {
+                self.end = self.end.offset(-1);
+                Some(&*self.end)
+            }
+        } else {
+            None
+        }
+    }
+}
+
+impl<'a, T> ExactSizeIterator for SliceIter<'a, T> { }
+
+impl<'a, T> From<&'a [T]> for SliceIter<'a, T> {
+    fn from(slice: &'a [T]) -> Self {
+        assert!(size_of::<T>() != 0);
+        unsafe {
+            let ptr = slice.as_ptr();
+            let end = ptr.offset(slice.len() as isize);
+            SliceIter::new(ptr, end)
+        }
+    }
+}
+
+impl<'a, T> Default for SliceIter<'a, T> {
+    /// Create an empty `SliceIter`.
+    fn default() -> Self {
+        unsafe {
+            SliceIter::new(0x1 as *const T, 0x1 as *const T)
+        }
+    }
+}
+
+impl<'a, T> Index<usize> for SliceIter<'a, T> {
     type Output = T;
     fn index(&self, i: usize) -> &T {
         assert!(i < self.len());
