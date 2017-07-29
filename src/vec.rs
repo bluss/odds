a867e2092b449c3e7586c862b0c24a9cba27934a
bluss <bluss@users.noreply.github.com>
2017-07-29 21:46:00 +0200

    Move VecExt::splice to separate function to not conflict with Vec::splice

--- src/vec.rs
+++ src/vec.rs
@@ -22,18 +22,6 @@ pub fn vec<I>(iterable: I) -> Vec<I::Item>
 ///
 /// Requires `feature="std"`
 pub trait VecExt<T> {
-    /// Remove elements in a range, and insert from an iterator in their place.
-    ///
-    /// The removed and inserted ranges don't have to match in length.
-    ///
-    /// **Panics** if range `r` is out of bounds.
-    ///
-    /// **Panics** if iterator `iter` is not of exact length.
-    fn splice<R, I>(&mut self, r: R, iter: I)
-        where I: IntoIterator<Item=T>,
-              I::IntoIter: ExactSizeIterator,
-              R: IndexRange;
-
     /// Retains only the elements specified by the predicate.
     ///
     /// In other words, remove all elements `e` such that `f(&mut e)` returns false.
@@ -65,62 +53,6 @@ pub trait VecExt<T> {
 ///
 /// **Panics** if iterator `iter` is not of exact length.
 impl<T> VecExt<T> for Vec<T> {
-    fn splice<R, I>(&mut self, r: R, iter: I)
-        where I: IntoIterator<Item=T>,
-              I::IntoIter: ExactSizeIterator,
-              R: IndexRange,
-    {
-        let v = self;
-        let mut iter = iter.into_iter();
-        let (input_len, _) = iter.size_hint();
-        let old_len = v.len();
-        let r = r.start().unwrap_or(0)..r.end().unwrap_or(old_len);
-        assert!(r.start <= r.end);
-        assert!(r.end <= v.len());
-        let rm_len = r.end - r.start;
-        v.reserve(input_len.saturating_sub(rm_len));
-
-        unsafe {
-            let ptr = v.as_mut_ptr();
-            v.set_len(r.start);
-
-            // drop all elements in `r`
-            {
-                let mslc = slice::from_raw_parts_mut(ptr.offset(r.start as isize), rm_len);
-                for elt_ptr in mslc {
-                    ptr::read(elt_ptr); // Possible panic
-                }
-            }
-
-            if rm_len != input_len {
-                // move tail elements
-                ptr::copy(ptr.offset(r.end as isize),
-                          ptr.offset((r.start + input_len) as isize),
-                          old_len - r.end);
-            }
-
-            // fill in elements from the iterator
-            // FIXME: On panic, drop tail properly too (using panic guard)
-            {
-                let grow_slc = slice::from_raw_parts_mut(ptr.offset(r.start as isize), input_len);
-                let mut len = r.start;
-                for slot_ptr in grow_slc {
-                    if let Some(input_elt) = iter.next() { // Possible Panic
-                        ptr::write(slot_ptr, input_elt);
-                    } else {
-                        // FIXME: Skip check with trusted iterators
-                        panic!("splice: iterator too short");
-                    }
-                    // update length to drop as much as possible on panic
-                    len += 1;
-                    v.set_len(len);
-                }
-                v.set_len(old_len - rm_len + input_len);
-            }
-        }
-        //assert!(iter.next().is_none(), "splice: iterator not exact size");
-    }
-
     // Adapted from libcollections/vec.rs in Rust
     // Primary author in Rust: Michael Darakananda
     fn retain_mut<F>(&mut self, mut f: F)
@@ -144,6 +76,73 @@ impl<T> VecExt<T> for Vec<T> {
         }
     }
 }
+
+/// Remove elements in a range, and insert from an iterator in their place.
+///
+/// The removed and inserted ranges don't have to match in length.
+///
+/// **Panics** if range `r` is out of bounds.
+///
+/// **Panics** if iterator `iter` is not of exact length.
+///
+/// *See also Vec's splice method, which is more flexible. As of this writing
+/// it is an experimental feature.*
+fn splice<T, R, I>(v: &mut Vec<T>, r: R, iter: I)
+    where I: IntoIterator<Item=T>,
+          I::IntoIter: ExactSizeIterator,
+          R: IndexRange
+{
+    let mut iter = iter.into_iter();
+    let (input_len, _) = iter.size_hint();
+    let old_len = v.len();
+    let r = r.start().unwrap_or(0)..r.end().unwrap_or(old_len);
+    assert!(r.start <= r.end);
+    assert!(r.end <= v.len());
+    let rm_len = r.end - r.start;
+    v.reserve(input_len.saturating_sub(rm_len));
+
+    unsafe {
+        let ptr = v.as_mut_ptr();
+        v.set_len(r.start);
+
+        // drop all elements in `r`
+        {
+            let mslc = slice::from_raw_parts_mut(ptr.offset(r.start as isize), rm_len);
+            for elt_ptr in mslc {
+                ptr::read(elt_ptr); // Possible panic
+            }
+        }
+
+        if rm_len != input_len {
+            // move tail elements
+            ptr::copy(ptr.offset(r.end as isize),
+                      ptr.offset((r.start + input_len) as isize),
+                      old_len - r.end);
+        }
+
+        // fill in elements from the iterator
+        // FIXME: On panic, drop tail properly too (using panic guard)
+        {
+            let grow_slc = slice::from_raw_parts_mut(ptr.offset(r.start as isize), input_len);
+            let mut len = r.start;
+            for slot_ptr in grow_slc {
+                if let Some(input_elt) = iter.next() { // Possible Panic
+                    ptr::write(slot_ptr, input_elt);
+                } else {
+                    // FIXME: Skip check with trusted iterators
+                    panic!("splice: iterator too short");
+                }
+                // update length to drop as much as possible on panic
+                len += 1;
+                v.set_len(len);
+            }
+            v.set_len(old_len - rm_len + input_len);
+        }
+    }
+    //assert!(iter.next().is_none(), "splice: iterator not exact size");
+    }
+
+
 
 pub trait VecFindRemove {
     type Item;
@@ -181,27 +180,27 @@ fn test_splice() {
     use std::iter::once;
 
     let mut v = vec![1, 2, 3, 4];
-    VecExt::splice(&mut v, 1..1, vec![9, 9]);
+    splice(&mut v, 1..1, vec![9, 9]);
     assert_eq!(v, &[1, 9, 9, 2, 3, 4]);
 
     let mut v = vec![1, 2, 3, 4];
-    VecExt::splice(&mut v, 1..2, vec![9, 9]);
+    splice(&mut v, 1..2, vec![9, 9]);
     assert_eq!(v, &[1, 9, 9, 3, 4]);
 
     let mut v = vec![1, 2, 3, 4, 5];
-    VecExt::splice(&mut v, 1..4, vec![9, 9]);
+    splice(&mut v, 1..4, vec![9, 9]);
     assert_eq!(v, &[1, 9, 9, 5]);
 
     let mut v = vec![1, 2, 3, 4];
-    VecExt::splice(&mut v, 0..4, once(9));
+    splice(&mut v, 0..4, once(9));
     assert_eq!(v, &[9]);
 
     let mut v = vec![1, 2, 3, 4];
-    VecExt::splice(&mut v, 0..4, None);
+    splice(&mut v, 0..4, None);
     assert_eq!(v, &[]);
 
     let mut v = vec![1, 2, 3, 4];
-    VecExt::splice(&mut v, 1.., Some(9));
+    splice(&mut v, 1.., Some(9));
     assert_eq!(v, &[1, 9]);
 }
 
